d4f3862361623d51c5b438e02000b83b8a7a57d4
irishabhk <rishabhk2626@gmail.com>
2022-12-12 12:48:12 +0530

    reverse array in array

--- first.py
+++ first.py
@@ -1,4 +1,13 @@
-def testFunction():
-    print('testing first github push...');
-
-testFunction();
+def reverse_array(start,end,A):
+    
+    while start<end:
+        A[start],A[end]=A[end],A[start]
+        start+=1
+        end-=1
+        
+    return
+A=["A","B","C",4,5,6]
+print(A)
+reverse_array(0,5,A)
+print("reverse of the array")
+print(A)
